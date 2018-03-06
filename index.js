de41c15bbe7651edb8a6756f164f8506943b99a9
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-03-06 05:06:27 +0100

    module: buble not the cjs version

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ var point = require('poltocar');
  * @example
  * poly(50, 8); // Octagon with opposite facing vertices 100 units apart
  */
-var poly = function (radius, sides) {
-  if ( radius === void 0 ) radius = 0;
-  if ( sides === void 0 ) sides = 3;
-
+const poly = (radius = 0, sides = 3) => {
   // Theta increment, compute once
-  var r = arithmetics.TAU / sides;
+  const r = arithmetics.TAU / sides;
 
-  return Array.from({ length: sides }).map(function (v, i) { return point(r * i, radius); })
+  return Array.from({ length: sides }).map((v, i) => point(r * i, radius))
 };
 
 /**
@@ -33,21 +30,16 @@ var poly = function (radius, sides) {
  * @returns {Array.<Object>} - Points x, y
  * @see {@link https://en.wikipedia.org/wiki/Archimedean_spiral}
  */
-var coil = function (radius, turns, phase, n) {
-  if ( radius === void 0 ) radius = 0;
-  if ( turns === void 0 ) turns = 1;
-  if ( phase === void 0 ) phase = 1;
-  if ( n === void 0 ) n = 1;
-
+const coil = (radius = 0, turns = 1, phase = 1, n = 1) => {
   // Decides type of spiral (eg. with Fermat's, n = 2)
-  var k = 1 / n;
+  const k = 1 / n;
 
   // Compute distance between turns
-  var r = radius / (arithmetics.TAU * turns);
+  const r = radius / (arithmetics.TAU * turns);
 
-  return Array.from({ length: 360 * turns }).map(function (v, i) {
-    var angle = arithmetics.rad(i);
-    var reach = phase + (r * Math.pow(angle, k));
+  return Array.from({ length: 360 * turns }).map((v, i) => {
+    const angle = arithmetics.rad(i);
+    const reach = phase + (r * Math.pow(angle, k));
 
     return point(angle, reach)
   })
@@ -62,21 +54,16 @@ var coil = function (radius, turns, phase, n) {
  * @returns {Array.<Object>} - Points x, y
  * @see {@link http://mathworld.wolfram.com/Rose.html}
  */
-var rose = function (radius, a, b, offset) {
-  if ( radius === void 0 ) radius = 0;
-  if ( a === void 0 ) a = 2;
-  if ( b === void 0 ) b = 3;
-  if ( offset === void 0 ) offset = 0;
-
+const rose = (radius = 0, a = 2, b = 3, offset = 0) => {
   // Decides number of petals
-  var k = a / b;
+  const k = a / b;
 
   // For calculating how many iterations produce a closed curve, assuming k is rational
-  var r = 2 - ((b * a) % 2);
+  const r = 2 - ((b * a) % 2);
 
-  return Array.from({ length: 180 * r * b }).map(function (v, i) {
-    var angle = arithmetics.rad(i);
-    var reach = radius * Math.cos(k * angle);
+  return Array.from({ length: 180 * r * b }).map((v, i) => {
+    const angle = arithmetics.rad(i);
+    const reach = radius * Math.cos(k * angle);
 
     return point(angle, reach + offset)
   })
@@ -95,35 +82,21 @@ var rose = function (radius, a, b, offset) {
  * @returns {Array.<Object>} - Points x, y
  * @see {@link http://paulbourke.net/geometry/supershape}
  */
-var foxy = function (radius, m1, n1, n2, n3, a, b, m2) {
-  if ( radius === void 0 ) radius = 0;
-  if ( m1 === void 0 ) m1 = 0;
-  if ( a === void 0 ) a = 1;
-  if ( b === void 0 ) b = a;
-  if ( m2 === void 0 ) m2 = m1;
+const foxy = (radius = 0, m1 = 0, n1, n2, n3, a = 1, b = a, m2 = m1) => {
+  const { cos, pow, abs } = Math;
 
-  var cos = Math.cos;
-  var pow = Math.pow;
-  var abs = Math.abs;
+  const input = [a, b, m1, m2, n2, n3];
+  const shift = arithmetics.TAU * 0.25;
 
-  var input = [a, b, m1, m2, n2, n3];
-  var shift = arithmetics.TAU * 0.25;
+  const score = (phi, t1 = 0, id = 0) => {
+    const [k, m, n] = input.filter((v, i) => ((i + id) % 2 ? 0 : v));
 
-  var score = function (phi, t1, id) {
-    if ( t1 === void 0 ) t1 = 0;
-    if ( id === void 0 ) id = 0;
-
-    var ref = input.filter(function (v, i) { return ((i + id) % 2 ? 0 : v); });
-    var k = ref[0];
-    var m = ref[1];
-    var n = ref[2];
-
-    var q = m * phi * 0.25;
+    let q = m * phi * 0.25;
 
     // Turn cos into sin
     q += id ? shift : 0;
 
-    var t2 = cos(q) / k;
+    let t2 = cos(q) / k;
 
     t2 = abs(t2);
     t2 = pow(t2, n);
@@ -135,10 +108,10 @@ var foxy = function (radius, m1, n1, n2, n3, a, b, m2) {
     return score(phi, t2, id + 1)
   };
 
-  return Array.from({ length: 360 }).map(function (v, i) {
-    var angle = arithmetics.rad(i);
-    var t = score(angle);
-    var reach = pow(t, 1 / n1);
+  return Array.from({ length: 360 }).map((v, i) => {
+    const angle = arithmetics.rad(i);
+    const t = score(angle);
+    const reach = pow(t, 1 / n1);
 
     return abs(reach) ? point(angle, radius / reach) : { x: 0, y: 0 }
   })
@@ -148,4 +121,3 @@ exports.poly = poly;
 exports.coil = coil;
 exports.rose = rose;
 exports.foxy = foxy;
-
